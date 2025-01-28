f31bb594bf7b6921abc51555598f80abe57244ec
RATIANANAHARY-Karmen-24024359 <karmen.ratiananahary@etu.univ-amu.fr>
2025-01-28 16:22:07 +0100

    Ajout du compilateur pour exo 1

--- src/Circle.java
+++ src/Circle.java
@@ -1,18 +1,50 @@
+
+
 public class Circle {
-	private double radius;
-	public Circle() {  
-	    radius = 2.0;
-	}
-	      
-	public Circle(double r) {  
-	    radius = r;
-	}
-	   
-	public double getRadius() {
-	    return radius; 
-	}
-	   
-	public double getArea() {
-	    return radius*radius*Math.PI;
+    private double radius;
+    private String color;
+    
+    public Circle() {
+        this.radius = 1.0;
+        this.color = "red";
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+        this.color = "red";
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+    
+    public String getColor() {
+        return this.color;
+    }
+  
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    
+    public void setColor(String color) {
+        this.color = color;
+    }
+   
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+  
+    public String toString() {
+        return "Circle[radius=" + radius + ", color=" + color + "]";
+    }
+
+	public Object getCircumference() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
