78f4fbc7cfe7957912c7a4f01e313bf9ecf85645
RATIANANAHARY-Karmen-24024359 <karmen.ratiananahary@etu.univ-amu.fr>
2025-01-28 15:50:48 +0100

    J'ai ajouté l'exercice 1 et modifié le nom du fichier Hello

---
+++ src/Circle.java
@@ -0,0 +1,18 @@
+public class Circle {
+	private double radius;
+	public Circle() {  
+	    radius = 2.0;
+	}
+	      
+	public Circle(double r) {  
+	    radius = r;
+	}
+	   
+	public double getRadius() {
+	    return radius; 
+	}
+	   
+	public double getArea() {
+	    return radius*radius*Math.PI;
+	}
+}
